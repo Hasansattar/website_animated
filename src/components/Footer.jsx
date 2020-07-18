4da3bb4d599899a9d992d303d136a547a15a51b2
hasansattar <hasansattar650@gmail.com>
2020-07-18 16:26:25 +0500

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
 function Footer() {
-    return (
+    return 
         
-        <>
-            <footer className="w-100 bg-light text-center footer">
-                <p>@ 2020 HASAN SATTAR. All rights reserved</p>
-            </footer>
-        </>
-    )
+         
+    
 }
 
 export default Footer
