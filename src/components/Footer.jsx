7b77fbbafd2e01b9ed97d8217d5121766c5cf6a9
hasansattar <hasansattar650@gmail.com>
2020-07-18 15:52:33 +0500

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         
         <>
-            <footer className="w-100 bg-light text-center">
+            <footer className="w-100 bg-light text-center footer">
                 <p>@ 2020 HASAN SATTAR. All rights reserved</p>
             </footer>
         </>
